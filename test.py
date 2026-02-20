b027c4e93a14e6accc075c4db00cdbd45f0de4cb
Zexuan Wu <wuzexuan@pku.edu.cn>
2026-02-20 16:51:17 +0100

    create new branch

--- test.py
+++ test.py
@@ -1,24 +1,16 @@
 from astropy.io import fits
 
-from oifits_schema import (GRAVITY_FT, OI_ARRAY, OI_FLUX, OI_T3, OI_VIS,
-                           OI_WAVELENGTH)
+from oifits_schema import GRAVITY_FT, OI
 
 with fits.open(
     "~/data/kb230025_ep1/reduced/GRAVI.2023-06-01T03:26:38.962_dualscivis.fits"
 ) as hdul:
-    oi_arr  = OI_ARRAY      (hdul)
-    oi_wave = OI_WAVELENGTH (hdul, GRAVITY_FT)
-    oi_flux = OI_FLUX       (hdul, GRAVITY_FT)
-    oi_vis  = OI_VIS        (hdul, GRAVITY_FT)
-    oi_t3   = OI_T3         (hdul, GRAVITY_FT)
-
-    print(oi_arr)
-    print(oi_t3)
-    print(oi_wave)
-    print(oi_vis)
+    oi = OI.load(hdul, GRAVITY_FT)
+    print(oi)
 
     import matplotlib.pyplot as plt
-
-    fig, ax = plt.subplots()
-    ax.plot(oi_wave.eff_wave, oi_t3.t3phi[::4, :].mean(axis=0))
+    plt.style.use("./zexwu.mplstyle")
+    fig, ax = plt.subplots(figsize=(6, 6))
+    for tri in range(4):
+        ax.plot(oi.wavelength.eff_wave*1e6, oi.t3.t3phi[tri::4, :].mean(axis=0), "o-")
     plt.show()
